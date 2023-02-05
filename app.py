1449e3135090ca55624f722efcc38b2dbfcdd43c
margauxvp <33750077+margauxvp@users.noreply.github.com>
2023-02-05 23:46:17 +0100

    Update app.py
    add imports

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from functools import wraps
 from flask import request, jsonify
 from bs4 import BeautifulSoup
 import os
