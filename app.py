7cabe71bf43c385e54bea1deccb739564030147a
margauxvp <33750077+margauxvp@users.noreply.github.com>
2023-02-09 18:39:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,20 @@ from azure.identity import DefaultAzureCredential
 # Initializing a Flask app instance
 app = Flask(__name__)
 
+# Setting up Azure Key Vault connection
+KVUri = f"https://openaibot-kv.vault.azure.net/"
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=KVUri, credential=credential)
+
+# Setting API Key and API endpoint for OpenAI
+openai.api_key = client.get_secret("teamsappsecret").value
+openai.api_base =  "https://openaijorge2.openai.azure.com/" 
+openai.api_type = 'azure'
+openai.api_version = '2022-12-01' 
+
+# Specifying deployment ID for OpenAI engine
+deployment_id='teams-davinci003' 
+
 @app.route('/')
 def index():
    print('Request for index page received')
